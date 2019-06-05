3a39a7b4ae24c86c6abe1983b1f4ab5750f574df
AChar95 <allancharles95@outlook.com>
2019-06-05 23:15:12 +0100

    Created Account class and associated test class

---
+++ src/main/java/com/qa/profiles/CharacterProfile.java
@@ -0,0 +1,53 @@
+package com.qa.profiles;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class CharacterProfile implements Profiles {
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	private int id;
+	private String firstName;
+	private String surname;
+	private String gameType;
+	private String className;
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+
+	}
+
+	public String getFirstName() {
+
+		return this.firstName;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public String getSurname() {
+		return this.surname;
+	}
+
+	public void setGameType(String gameType) {
+		this.gameType = gameType;
+	}
+	public String getGameType() {
+		return this.gameType;
+	}
+
+	public void setClassName(String className) {
+		this.className = className;
+	}
+	public String getClassName() {
+		return this.className;
+	}
+	public int getId() {
+		return this.id;
+	}
+
+}
