683b54a7daf32fd995dd173e78201c267f16e26c
AChar95 <allancharles95@outlook.com>
2019-06-16 17:14:40 +0100

    project running correctly

--- src/main/webapp/JScripts/index.js
+++ src/main/webapp/JScripts/index.js
@@ -3,18 +3,14 @@ const setUserName = (name) => { username = name.value };
 
 
 function submit() {
-    fetchData("", "GET", "/users").then((value) => {
+    fetchData("", "GET", "/username/" + username).then((value) => {
         let user = JSON.parse(value);
-        let pinUse = user.filter(function (user) {
-            return user.username === username;
-        }) 
-        if (pinUse.length == 1) {
-            let userData = pinUse[0];
-            let data = { number: userData["id"] };
-            sessionStorage.setItem("Data", JSON.stringify(data));
-            window.location.assign("Home.html");
-        } else {
-            sessionStorage.setItem("tempName", username);
-            window.location.assign("register.html");
-        }}).catch((error) => console.log(error));
+        let userData = user[0];
+        let data = { number: userData["id"] };
+        sessionStorage.setItem("Data", JSON.stringify(data));
+        window.location.assign("Home.html");
+    }).catch(() => {
+        sessionStorage.setItem("tempName", username);
+        window.location.assign("register.html")
+    });
 };
